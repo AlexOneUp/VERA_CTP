a7f75f0bd60704727b55bcc2578d84160c5a45ce
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2022-11-24 08:10:21 -0500

    Finalized Front End UI and Fixed Back End Bugs

--- backend/audio_processing.py
+++ backend/audio_processing.py
@@ -1,24 +1,33 @@
+import librosa as ls
 import numpy as np
 import pandas as pd
-from statistics import mode 
-import librosa as ls
 
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from keras.utils import np_utils 
-from sklearn.model_selection import train_test_split 
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from statistics import mode
 from tensorflow.keras.models import load_model
 
-model_path = os.getenv('model_path')
-X_path = os.getenv('X_path')
-y_path = os.getenv('y_path')
+model_path = os.getenv("model_path")
+X_path = os.getenv("X_path")
+y_path = os.getenv("y_path")
 
 # List of emotions the model was trained on.
-emotions_classes = ['angry','calm', 'disgust','fear',
-                    'happy','neutral', 'sad','surprise']
+emotions_classes = [
+    "angry",
+    "calm",
+    "disgust",
+    "fear",
+    "happy",
+    "neutral",
+    "sad",
+    "surprise",
+]
 
 # Load the model.
 model = load_model(model_path)
@@ -30,13 +39,17 @@ X = pd.read_parquet(X_path)
 y = pd.read_parquet(y_path)
 
 # Convert class vector (integers) to binary class matrix.
-label = LabelEncoder() 
+label = LabelEncoder()
 y = y.squeeze()
 y = np_utils.to_categorical(label.fit_transform(y))
 
 # Split data into training, validation, and testing.
-X_train, X_remain, y_train, y_remain = train_test_split(X, y, test_size=0.2, random_state=42)
-X_valid, X_test, y_valid, y_test = train_test_split(X_remain, y_remain, test_size=0.5, random_state=42)
+X_train, X_remain, y_train, y_remain = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+X_valid, X_test, y_valid, y_test = train_test_split(
+    X_remain, y_remain, test_size=0.5, random_state=42
+)
 
 # Standardize / Scale data.
 # Standardize features by removing the mean and scaling to unit variance.
@@ -48,94 +61,115 @@ X_test = standard_scaler.transform(X_test.values)
 
 # Next 4 functions are Audio Data Augmentation:
 # Noise Injection
-def inject_noise(data, random=False, rate = 0.035, threshold = 0.075):
-    if random: rate = np.random.random() * threshold
+def inject_noise(data, random=False, rate=0.035, threshold=0.075):
+    if random:
+        rate = np.random.random() * threshold
 
     noise_amplitude = rate * np.random.uniform() * np.amax(data)
-    augmented_data = data + noise_amplitude * np.random.normal(size = data.shape[0])
+    augmented_data = data + noise_amplitude * np.random.normal(size=data.shape[0])
 
     return augmented_data
 
+
 # Pitching
-def pitching(data, sampling_rate, pitch_factor = 0.7, random=False):
-    if random: pitch_factor= np.random.random() * pitch_factor
+def pitching(data, sampling_rate, pitch_factor=0.7, random=False):
+    if random:
+        pitch_factor = np.random.random() * pitch_factor
     return ls.effects.pitch_shift(y=data, sr=sampling_rate, n_steps=pitch_factor)
 
-def streching(data,rate = 0.8): # Stretching
+
+# Stretching
+def streching(data, rate=0.8):
     return ls.effects.time_stretch(y=data, r=rate)
 
+
 # Zero crossing rate
-def zero_crossing_rate(data,frame_length, hop_length):
-    zcr = ls.feature.zero_crossing_rate(y=data, frame_length=frame_length, hop_length=hop_length)
+def zero_crossing_rate(data, frame_length, hop_length):
+    zcr = ls.feature.zero_crossing_rate(
+        y=data, frame_length=frame_length, hop_length=hop_length
+    )
     return np.squeeze(zcr)
 
+
 # Root mean square
-def root_mean_square(data, frame_length = 2048, hop_length = 512):
+def root_mean_square(data, frame_length=2048, hop_length=512):
     rms_num = ls.feature.rms(y=data, frame_length=frame_length, hop_length=hop_length)
     return np.squeeze(rms_num)
 
+
 # Mel frequency cepstral coefficients
-def mel_frequency_cepstral_coefficients(data, sampling_rate,
-                                        frame_length=2048,
-                                        hop_length=512,
-                                        flatten: bool=True):
+def mel_frequency_cepstral_coefficients(
+    data, sampling_rate, frame_length=2048, hop_length=512, flatten: bool = True
+):
     mfcc = ls.feature.mfcc(y=data, sr=sampling_rate)
-    
+
     return np.squeeze(mfcc.T) if not flatten else np.ravel(mfcc.T)
+
 
 # Combined audio data feature extraction.
 def feature_extraction(data, sampling_rate, frame_length=2048, hop_length=512):
     result = np.array([])
-    result = np.hstack((result,
-        ls.feature.zero_crossing_rate(data, frame_length, hop_length),
-        root_mean_square(data, frame_length, hop_length),
-        mel_frequency_cepstral_coefficients(data, sampling_rate, frame_length, hop_length)
-    ))
+    result = np.hstack(
+        (
+            result,
+            zero_crossing_rate(data, frame_length, hop_length),
+            root_mean_square(data, frame_length, hop_length),
+            mel_frequency_cepstral_coefficients(
+                data, sampling_rate, frame_length, hop_length
+            ),
+        )
+    )
     return result
+
 
 # Duration and offset act as placeholders because there is no audio in start and the ending of
 # each audio file is normally below three seconds.
 # Combine audio data augmentation and audio data feature extraction.
-def get_features(file_path, duration = 2.5, offset = 0.6):
+def get_features(file_path, duration=2.5, offset=0.6):
     data, sampling_rate = ls.load(file_path, duration=duration, offset=offset)
-    
+
     # No audio data augmentation.
     audio_1 = feature_extraction(data, sampling_rate)
     audio = np.array(audio_1)
-    
+
     # Inject Noise.
-    noise_audio = inject_noise(data, random = True)
-    audio_2 =  feature_extraction(noise_audio, sampling_rate)
+    noise_audio = inject_noise(data, random=True)
+    audio_2 = feature_extraction(noise_audio, sampling_rate)
     audio = np.vstack((audio, audio_2))
-    
+
     # Pitching.
-    pitch_audio = pitching(data, sampling_rate, random = True)
+    pitch_audio = pitching(data, sampling_rate, random=True)
     audio_3 = feature_extraction(pitch_audio, sampling_rate)
     audio = np.vstack((audio, audio_3))
-    
+
     # Pitching and Inject Noise.
-    pitch_audio_1 = pitching(data, sampling_rate, random = True)
-    pitch_noise_audio = inject_noise(pitch_audio_1, random = True)
+    pitch_audio_1 = pitching(data, sampling_rate, random=True)
+    pitch_noise_audio = inject_noise(pitch_audio_1, random=True)
     audio_4 = feature_extraction(pitch_noise_audio, sampling_rate)
     audio = np.vstack((audio, audio_4))
-    
+
     audio_features = audio
-    
+
     return audio_features
+
 
 # Increase ndarray dimensions to [4,2376].
 def increase_ndarray_size(features_test):
     tmp = np.zeros([4, 2377])
     offsets = [0, 1]
-    insert_here = tuple([slice(offsets[dim],
-                        offsets[dim] + features_test.shape[dim])
-                        for dim in range(features_test.ndim)])
-    
+    insert_here = tuple(
+        [
+            slice(offsets[dim], offsets[dim] + features_test.shape[dim])
+            for dim in range(features_test.ndim)
+        ]
+    )
+
     tmp[insert_here] = features_test
     features_test = tmp
     features_test = np.delete(features_test, 0, axis=1)
-    
+
     return features_test
+
 
 # Determine if ndarray needs to be increase in size.
 def increase_array_size(audio_features):
@@ -143,18 +177,20 @@ def increase_array_size(audio_features):
         audio_features = increase_ndarray_size(audio_features)
     return audio_features
 
+
 # Make the prediction.
 def predict(audio_features):
     audio_features = standard_scaler.transform(audio_features)
-    audio_features = np.expand_dims(audio_features, axis = 2)
+    audio_features = np.expand_dims(audio_features, axis=2)
 
     y_pred = model.predict(audio_features)
-    y_pred = np.argmax(y_pred, axis = 1)
-    
+    y_pred = np.argmax(y_pred, axis=1)
+
     try:
-        print('\nPredicted emotion for each and every feature extraction.\n\n', y_pred)
-        print('\nAvailable emotions_classes = ', emotions_classes)
-        print('\nModel predicted emotion: ', emotions_classes[mode(y_pred)])
+        print("\nPredicted emotion for each and every feature extraction.\n\n", y_pred)
+        print("\nAvailable emotions_classes = ", emotions_classes)
+        print("\nModel predicted emotion: ", emotions_classes[mode(y_pred)])
         return emotions_classes[mode(y_pred)]
 
-    except: return emotions_classes[y_pred[0]]
+    except:
+        return emotions_classes[y_pred[0]]
--- frontend/app.py
+++ frontend/app.py
@@ -1,45 +1,89 @@
+import os
 import random
 import streamlit as st
+import sys
+
+from dotenv import load_dotenv
 from sounddevice import rec, wait
 from scipy.io.wavfile import write
 
-import os
-import sys
-from dotenv import load_dotenv
+
 load_dotenv()
 
 # Absolute paths must be used.
-backend_path = os.getenv('backend_path')
-project_path = os.getenv('project_path')
+backend_path = os.getenv("backend_path")
+project_path = os.getenv("project_path")
 sys.path.append(backend_path)
 from audio_processing import get_features, increase_array_size, predict
 
-def local_css(file_name): # Use local CSS
-  with open(file_name) as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("styles/style.css") 
+
+def local_css(file_name):  # Use local CSS
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("styles/style.css")
 
 # Prompts used in training data.
-prompts = ['Kids are talking by the door', 'Dogs are sitting by the door',
-'It\'s eleven o\'clock', 'That is exactly what happened', 'I\'m on my way to the meeting',
-'I wonder what this is about', 'The airplane is almost full', 'Maybe tomorrow it will be cold',
-'I think I have a doctor\'s appointment', 'Say the word apple']
+prompts = [
+    "Kids are talking by the door",
+    "Dogs are sitting by the door",
+    "It's eleven o'clock",
+    "That is exactly what happened",
+    "I'm on my way to the meeting",
+    "I wonder what this is about",
+    "The airplane is almost full",
+    "Maybe tomorrow it will be cold",
+    "I think I have a doctor's appointment",
+    "Say the word apple",
+]
 
 # emotions = ['angry 😡', 'calm 😌', 'disgusted 🤢', 'fearful 😨', 'happy 😆',
-          # 'neutral 🙂', 'sad 😢', 'surprised 😳']
+# 'neutral 🙂', 'sad 😢', 'surprised 😳']
 
-emotion_dict = {'angry': 'angry 😡', 'calm': 'calm 😌',
-               'disgust': 'disgusted 🤢', 'fear': 'scared 😨',
-               'happy': 'happy 😆', 'neutral': 'neutral 🙂',
-               'sad': 'sad 😢', 'surprise': 'surprised 😳'}
+emotion_dict = {
+    "angry": "angry 😡",
+    "calm": "calm 😌",
+    "disgust": "disgusted 🤢",
+    "fear": "feared 😨",
+    "happy": "happy 😆",
+    "neutral": "neutral 🙂",
+    "sad": "sad 😢",
+    "surprise": "surprised 😳",
+}
 
-emotion = random.choice(list(emotion_dict)) 
-partition = emotion_dict.get(emotion).split(' ')
-particle = partition[1]
+# Session states.
+if "initial_styling" not in st.session_state:
+    st.session_state["initial_styling"] = True
 
-def styling(): # model for 'snowfall' animation 
-  return st.markdown(
-    f"""
+if "particle" not in st.session_state:
+    st.session_state["particle"] = "👋🏻"
+
+if "prompt" not in st.session_state:
+    st.session_state["prompt"] = ""
+
+if "emotion" not in st.session_state:
+    st.session_state["emotion"] = ""
+
+if "is_prompt" not in st.session_state:
+    st.session_state["is_prompt"] = False
+
+if "is_emotion" not in st.session_state:
+    st.session_state["is_emotion"] = False
+
+if "is_first_time_prompt" not in st.session_state:
+    st.session_state["is_first_time_prompt"] = True
+
+
+# emotion = random.choice(list(emotion_dict))
+# partition = emotion_dict.get(emotion).split(" ")
+
+
+def styling(particle):  # model for 'snowfall' animation
+    return st.markdown(
+        f"""
+      <div class="snowflake">{particle}</div>
+      <div class="snowflake">{particle}</div>
       <div class="snowflake">{particle}</div>
       <div class="snowflake">{particle}</div>
       <div class="snowflake">{particle}</div>
@@ -49,8 +93,10 @@ def styling(): # model for 'snowfall' animation
         <div class='wave -two'></div>
         <div class='wave -three'></div>
       </div>
-    """, unsafe_allow_html=True,
-  )
+    """,
+        unsafe_allow_html=True,
+    )
+
 
 # def card():
 #   return """
@@ -75,92 +121,254 @@ def styling(): # model for 'snowfall' animation
 #       </div>
 #     </div>
 #   """
-# 
-styling()
+#
 
-# Bootstrap cards w/ reference to css
-st.markdown("""
+
+# Bootstrap cards w/ reference to CSS.
+st.markdown(
+    """
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
   integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  """, unsafe_allow_html=True
+  """,
+    unsafe_allow_html=True,
 )
+
 
 # st.markdown(card(), unsafe_allow_html=True)
 
+
+def make_grid(rows, cols):
+    grid = [0] * rows
+    for i in range(rows):
+        with st.container():
+            grid[i] = st.columns(cols)
+    return grid
+
+
 # Title.
-st.write('# Voice Emotion Recognition on Audio')
+title = '<p align="center" style="font-family: monospace; color: #040428; font-size: 2.3rem;">Voice Emotion Recognition on Audio</p>'
+st.markdown(title, unsafe_allow_html=True)
 
 # Image.
-image = 'https://t4.ftcdn.net/jpg/03/27/36/95/360_F_327369570_CAxxxHHLvjk6IJ3wGi1kuW6WTtqjaMpc.jpg'
+image = "https://t4.ftcdn.net/jpg/03/27/36/95/360_F_327369570_CAxxxHHLvjk6IJ3wGi1kuW6WTtqjaMpc.jpg"
 st.image(image, use_column_width=True)
 
 # Header.
-header = 'We will randomly choose a prompt for you to read:'
-st.header(header)
-prompt = '"' + random.choice(prompts) + '"'
-st.subheader(prompt)
+header = '<p align="center" style="font-family: monospace; color: #040428; font-size: 1.7rem;">Click to generate a random prompt and emotion:</p>'
+st.markdown(header, unsafe_allow_html=True)
 
-# prompting emotion to user
-emotion_prompt = "Try to sound " + emotion_dict.get(emotion) + ":"
-st.subheader(emotion_prompt)
 
+def prompt_btn():
+    if not (st.session_state["is_first_time_prompt"]):
+        styling(particle=st.session_state["particle"])
+
+    prompt = '"' + random.choice(prompts) + '"'
+    st.session_state["prompt"] = prompt
+
+    st.markdown(
+        f"""
+            <p align="center" style="font-family: monospace; color: #ffffff; font-size: 2rem;">{st.session_state["prompt"]}</p>
+            """,
+        unsafe_allow_html=True,
+    )
+
+    if not (st.session_state["is_first_time_prompt"]):
+        st.markdown(
+            f"""
+                <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">Try to sound {emotion_dict.get(st.session_state["emotion"])}</p>
+                """,
+            unsafe_allow_html=True,
+        )
+    else:
+        st.markdown(
+            f"""
+                <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">Please generate an emotion!</p>
+                """,
+            unsafe_allow_html=True,
+        )
+
+
+def emotion_btn():
+    st.session_state["initial_styling"] = False
+    st.session_state["is_first_time_prompt"] = False
+
+    emotion = random.choice(list(emotion_dict))
+    st.session_state["emotion"] = emotion
+
+    partition = emotion_dict.get(emotion).split(" ")
+    particle = partition[1]
+    st.session_state["particle"] = particle
+    styling(particle=st.session_state["particle"])
+
+    st.markdown(
+        f"""
+            <p align="center" style="font-family: monospace; color: #ffffff; font-size: 2rem;">{st.session_state["prompt"]}</p>
+            """,
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        f"""
+            <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">Try to sound {emotion_dict.get(st.session_state["emotion"])}</p>
+            """,
+        unsafe_allow_html=True,
+    )
+
+
+# Record button.
 def record_btn():
-  if st.button('Record'): # Record audio 
     fs = 44100  # Sample rate.
-    seconds = 3 # Duration of recording.
-  
-    with st.spinner(f'Recording for {seconds} seconds ....'):
-      myrecording = rec(int(seconds * fs), samplerate = fs, channels = 1)
-      wait() # Wait until recording is finished.
-    
-      write(project_path + 'frontend/soundfiles/recording.wav', fs, myrecording) # Save as .wav file.
-      st.success('Recording completed.')
+    seconds = 3  # Duration of recording.
 
-def play_btn(): # Play the recorded audio.
-  if st.button('Play'):
-    try: # Load audio file.
-      audio_file = open(project_path + 'frontend/soundfiles/recording.wav', 'rb')
-      audio_bytes = audio_file.read()
-      st.audio(audio_bytes) 
-    
-    except: st.write('Please record sound first')
+    with st.spinner(f"Recording for {seconds} seconds ...."):
+        myrecording = rec(int(seconds * fs), samplerate=fs, channels=1)
+        wait()  # Wait until recording is finished.
 
+        write(
+            project_path + "frontend/soundfiles/recording.wav", fs, myrecording
+        )  # Save as .wav file.
+        st.success("Recording completed.")
+
+
+# Play button.
+def play_btn():  # Play the recorded audio.
+    styling(particle=st.session_state["particle"])
+    st.markdown(
+        f"""
+            <p align="center" style="font-family: monospace; color: #ffffff; font-size: 2rem;">{st.session_state["prompt"]}</p>
+            """,
+        unsafe_allow_html=True,
+    )
+    if not (st.session_state["is_first_time_prompt"]):
+        st.markdown(
+            f"""
+                <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">Try to sound {emotion_dict.get(st.session_state["emotion"])}</p>
+                """,
+            unsafe_allow_html=True,
+        )
+    else:
+        st.markdown(
+            f"""
+                <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">Please generate a prompt and an emotion!</p>
+                <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">Then, record your audio.</p>
+                """,
+            unsafe_allow_html=True,
+        )
+    try:  # Load audio file.
+        audio_file = open(project_path + "frontend/soundfiles/recording.wav", "rb")
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes)
+    except:
+        st.write("Please record sound first.")
+
+
+# Classify button.
 def classify_btn():
-  if st.button('Classify'):
-    try: 
-      wav_path = project_path + 'frontend/soundfiles/recording.wav'
-      audio_features = get_features(wav_path, 3)
-      audio_features = increase_array_size(audio_features)
-      emotion = predict(audio_features)
-      
-      result = emotion_dict.get(emotion)
-      st.write(emotion)
-    
-    except: st.write('Something went wrong. Please try again')
+    try:
+        wav_path = project_path + "frontend/soundfiles/recording.wav"
+        audio_features = get_features(wav_path, 3)
+        audio_features = increase_array_size(audio_features)
+        emotion = predict(audio_features)
 
-# UI design.
-col1, col2, col3 = st.columns(3)
-with col1: record_btn()
-with col2: play_btn()
-with col3: classify_btn()
-  
-# Skeleton on Predicted Value of the audio. 
+        if st.session_state["emotion"] != "":
+            if emotion in st.session_state["emotion"]:
+                st.session_state["particle"] = "😆"
+                styling(particle=st.session_state["particle"])
+                st.markdown(
+                    f"""
+                        <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">You tried to sound {st.session_state["emotion"].upper()} and you sounded {emotion.upper()}</p>
+                        <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">Well done!👍</p>
+                        """,
+                    unsafe_allow_html=True,
+                )
+                try:  # Load audio file.
+                    audio_file = open(
+                        project_path + "frontend/soundfiles/recording.wav", "rb"
+                    )
+                    audio_bytes = audio_file.read()
+                    st.audio(audio_bytes)
+                except:
+                    st.write("Please record sound first.")
+                st.balloons()
+            else:
+                st.session_state["particle"] = "😢"
+                styling(particle=st.session_state["particle"])
+                st.markdown(
+                    f"""
+                        <p align="center" style="font-family: monospace; color: #040428; font-size: 2.5rem;">You tried to sound {st.session_state["emotion"].upper()} however you sounded {emotion.upper()}👎</p>
+                        """,
+                    unsafe_allow_html=True,
+                )
+                try:  # Load audio file.
+                    audio_file = open(
+                        project_path + "frontend/soundfiles/recording.wav", "rb"
+                    )
+                    audio_bytes = audio_file.read()
+                    st.audio(audio_bytes)
+                except:
+                    st.write("Please record sound first.")
+    except:
+        st.write("Something went wrong. Please try again.")
+
+
+# User Interface.
+if st.session_state["initial_styling"]:
+    styling(particle=st.session_state["particle"])
+
+# Create custom grid.
+grid1 = make_grid(1, 14)
+
+# Prompt Button.
+prompt = grid1[0][5].button("Prompt")
+if prompt or st.session_state["is_prompt"]:
+    st.session_state["is_emotion"] = False
+    prompt_btn()
+
+# Emotion Button.
+emotion = grid1[0][7].button("Emotion")
+if emotion or st.session_state["is_emotion"]:
+    st.session_state["is_prompt"] = False
+    emotion_btn()
+
+# Create custom grid.
+grid2 = make_grid(3, (12, 12, 3))
+
+
+# Record Button.
+record = grid2[0][0].button("Record")
+if record:
+    record_btn()
+
+# Play Button.
+play = grid2[0][1].button("Play")
+if play:
+    play_btn()
+
+# Classify Button.
+classify = grid2[0][2].button("Classify")
+if classify:
+    classify_btn()
+
+# GitHub repository of project.
+st.markdown(
+    f"""
+    <p align="center" style="font-family: monospace; color: #000000; font-size: 1rem;"><b> Check out our
+        <a href="https://github.com/Alexoneup/VERA_CTP" style="color: #000000;"> GitHub repository</a></b>
+    </p>
+   """,
+    unsafe_allow_html=True,
+)
+
+
+# Skeleton on Predicted Value of the audio.
 # def state_emotion():
-  
+
 #   # In the case of persistent emotion data from a previous session:
 #   try:
 #     st.write("Are you still", st.session_state.emotion, "?")
-  
+
 # # Prompt 'Yes' or 'No' buttons
 #   # Then let's you predict again
 
 # # Show the Predict Emotion first.
 #   except:
 #     play = st.button('Predict Emotion', key='emotion', on_click=state_emotion)
-
-repo = """<br>
-          <p align="right"> Check out our 
-            <a href="https://github.com/Alexoneup/VERA_CTP"> GitHub repository </a>
-          </p>
-        """
-st.write(repo, unsafe_allow_html=True)
